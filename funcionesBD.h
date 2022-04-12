c016f0a075c1effc24cfa732be4817325634abe0
XabiGomez <xabier.gomezu@opendeusto.es>
2022-04-12 19:45:06 +0200

    Estructuras

--- funcionesBD.h
+++ funcionesBD.h
@@ -29,6 +29,34 @@ typedef struct
 }ListaPais;
 
 
+typedef struct 
+{
+    int cd_lan;
+    float distancia;
+}Lanzamiento;
+
+typedef struct 
+{
+    int cd_mod;
+    char descripcion[200];
+    char nom_modalidad [40];
+}Modalidad;
+
+typedef struct 
+{
+    int cdC;
+    char organizador[40];
+    char nombre[100];                                                    
+}Competicion;
+
+typedef struct 
+{
+    Persona* personas;
+    Modalidad* modalidades;
+    Competicion* competicines;
+    Lanzamiento* Lanzamiento;
+}Ranking;//Tabla compite
+
 
 int conectarBase(char base[],sqlite3* db);
 int cargarAtletas(sqlite3 *db, ListaPersona* lper);
